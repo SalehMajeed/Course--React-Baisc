5b51c7c3311b0fff1873234130304e8b49f64943
salehmajeed <saleh.majeed8@gmail.com>
2022-04-03 17:40:38 +0530

    added state

--- src/Components/TextFrom.js
+++ src/Components/TextFrom.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function TextFrom(props) {
+const [text, setText] = useState('Enter the Text here');
+const handleUpperCase = () => {
+    const newText = text.trim().toUpperCase();
+    setText(prevText => newText)
+}
+const handleOnChange = (event) => {
+    setText(prevText =>  event.target.value)
+}
   return (
     <>
     <h1>{props.heading}</h1>
@@ -9,9 +17,11 @@ export default function TextFrom(props) {
           className="form-control"
           id="myBox"
           rows="8"
+          value={text}
+          onChange={handleOnChange}
         ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to UpperCase</button>
+      <button className="btn btn-primary" onClick={handleUpperCase}>Convert to UpperCase</button>
     </>
   );
 }
