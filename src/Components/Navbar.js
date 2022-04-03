510c07e1bcec26d1a32a263abe16903e9980e682
salehmajeed <saleh.majeed8@gmail.com>
2022-04-03 15:43:51 +0530

    added propTypes and defaultProps

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
@@ -47,3 +48,5 @@ export default function Navbar(props) {
     </nav>
   );
 }
+Navbar.propTypes = { title: PropTypes.string.isRequired, aboutText: PropTypes.string.isRequired };
+Navbar.defaultProps = { title:'Title Text', aboutText:'about text'}
