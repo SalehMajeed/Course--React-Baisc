132ffa61a4de0460570ca321b43a415b18d4c02e
salehmajeed <saleh.majeed8@gmail.com>
2022-04-03 15:58:58 +0530

    added button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import TextFrom from "./Components/TextFrom";
 
 function App() {
   return (
     <>
-      <Navbar title='TextUtils' aboutText='About US' />
+      <Navbar title="TextUtils" aboutText="About US" />
       {/* <Navbar   /> */}
+      <div className="container my-3">
+        <TextFrom heading="Enter Your Text to analyze below"/>
+      </div>
     </>
   );
 }
